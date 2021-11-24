c11da0a93c972d8fcf547783f0af11839370e662
Toms Druvaskalns <tomsdruvas@gmail.com>
2021-11-24 14:42:42 +0000

    fixed buy_drink function

--- pub_lab/src/customer.py
+++ pub_lab/src/customer.py
@@ -1,11 +1,30 @@
+from src.pub import Pub
+from src.drink import Drink
+import pdb
+
 class Customer:
-    def __init__(self, name, wallet, finished_drinks):
+    def __init__(self, name, wallet):
         self.name = name
         self.wallet = wallet
-        self.finished_drinks = finished_drinks
+        self.finished_drinks = []
+        self.drink1 = Drink("Stella", 5, True)
+        self.drink2 = Drink("Gin and Tonic", 6, True)
+        self.drink3 = Drink("Tea", 4, False)
+        self.drinks_list = [self.drink1, self.drink2, self.drink3]
 
     def reduce_wallet(self, amount):
         self.wallet -= amount 
 
-    def add_finished_drink(self, amount):
-        self.finished_drinks += amount
+    def add_finished_drink(self, drink):
+        self.finished_drinks.append(drink)
+
+    def buy_drink(self, name_of_drink, pub):
+        for drink in pub.drinks_list:
+            if drink.name == name_of_drink:
+                # pdb.set_trace()
+                self.reduce_wallet(drink.price)
+                pub.increase_cash(drink.price)
+                self.add_finished_drink(drink)
+                pub.remove_drink(drink)
+        
+
--- pub_lab/tests/customer_test.py
+++ pub_lab/tests/customer_test.py
@@ -1,14 +1,23 @@
 import unittest
 from src.customer import Customer
+from src.pub import Pub
+from src.drink import Drink
+from tests.pub_test import TestPub
 
 class TestCustomer(unittest.TestCase):
 
     def setUp(self):
-        self.customer = Customer("Joe", 500, 0)
+        self.drink1 = Drink("Stella", 5, True)
+        self.drink2 = Drink("Gin and Tonic", 6, True)
+        self.drink3 = Drink("Tea", 4, False)
+        self.drinks_list = [self.drink1, self.drink2, self.drink3]
+        self.customer = Customer("Joe", 500)
+        self.pub = Pub("The Prancing Pony", 100.00, self.drinks_list)
+
 
     def test_check_finished_drinks(self):
         expected = 0
-        result = self.customer.finished_drinks
+        result = len(self.customer.finished_drinks)
         self.assertEqual(expected, result)
 
     def test_reduce_wallet(self):
@@ -18,5 +27,16 @@ class TestCustomer(unittest.TestCase):
 
     def test_add_finished_drinks(self):
         expected = 1
-        self.customer.add_finished_drink(1)
-        self.assertEqual(expected, self.customer.finished_drinks)
+        self.customer.add_finished_drink(self.drink1)
+        self.assertEqual(expected, len(self.customer.finished_drinks))
+
+    def test_buy_drink(self):
+        self.customer.buy_drink("Stella", self.pub)
+        # reduce cash of customer
+        self.assertEqual(495, self.customer.wallet)
+        # increase cash in till
+        self.assertEqual(105, self.pub.cash)
+        # increase customers finished drinks
+        self.assertEqual(1, len(self.customer.finished_drinks))
+        # remove from pub drinks list
+        self.assertEqual(2, self.pub.drink_stock())
